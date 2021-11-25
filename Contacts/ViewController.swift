b374d7d26731608c9ed884b1334ca2848b05dd96
devGod <devgod@mail.ru>
2021-11-25 19:18:43 +0300

    initial commit

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -13,7 +13,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell: UITableViewCell
+        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "MyCell") {
+            print("Использую старую ячейку для строки с индексом \(indexPath.row)")
+            cell = reuseCell
+        } else {
+            print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+            cell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+        }
+        configure(cell: &cell, for: indexPath)
+        return cell
+    }
+
+    private func configure(cell: inout UITableViewCell, for indexPath: IndexPath) {
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Строка \(indexPath.row)"
+        cell.contentConfiguration = configuration
+    }
+}
+
 
